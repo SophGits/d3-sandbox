06f54e01453568c9ce14731d166ba03d25a2ab6b
Sophie Chitty <sophie@builtvisible.com>
2016-02-18 12:18:35 +0000

    Append lines to chart.

--- categorychart_test/script-2.js
+++ categorychart_test/script-2.js
@@ -59,11 +59,11 @@ var data = [
           'days': {
             '1': 1,
             '2': 21,
-            '3': 11,
-            '4': 11,
-            '5': 11,
-            '6': 11,
-            '7': 14,
+            '3': 21,
+            '4': 21,
+            '5': 21,
+            '6': 18,
+            '7': 15,
             '8': 11
           },
           'most-popular': {
@@ -87,13 +87,13 @@ var data = [
         'data': {
           'days': {
             '1': 1,
-            '2': 21,
-            '3': 11,
+            '2': 23,
+            '3': 31,
             '4': 11,
-            '5': 11,
-            '6': 11,
-            '7': 14,
-            '8': 11
+            '5': 51,
+            '6': 41,
+            '7': 54,
+            '8': 8
           },
           'most-popular': {
             'name': 'Henry',
@@ -110,12 +110,12 @@ var data = [
           'days': {
             '1': 1,
             '2': 21,
-            '3': 11,
-            '4': 11,
-            '5': 11,
-            '6': 11,
-            '7': 14,
-            '8': 11
+            '3': 31,
+            '4': 29,
+            '5': 28,
+            '6': 26,
+            '7': 24,
+            '8': 21
           },
           'most-popular': {
             'name': 'Pope',
@@ -130,14 +130,14 @@ var data = [
       'pumpkin': {
         'data': {
           'days': {
-            '1': 1,
-            '2': 21,
-            '3': 11,
-            '4': 11,
-            '5': 11,
-            '6': 11,
-            '7': 14,
-            '8': 11
+            '1': 4,
+            '2': 6,
+            '3': 10,
+            '4': 5,
+            '5': 8,
+            '6': 10,
+            '7': 11,
+            '8': 9
           },
           'most-popular': {
             'name': 'Bo',
@@ -157,14 +157,14 @@ var data = [
       'chickpea': {
         'data': {
           'days': {
-            '1': 1,
+            '1': 12,
             '2': 21,
-            '3': 11,
-            '4': 11,
+            '3': 17,
+            '4': 41,
             '5': 11,
-            '6': 11,
-            '7': 14,
-            '8': 11
+            '6': 61,
+            '7': 34,
+            '8': 19
           },
           'most-popular': {
             'name': 'Finch',
@@ -179,14 +179,14 @@ var data = [
       'lentil': {
         'data': {
           'days': {
-            '1': 1,
-            '2': 21,
-            '3': 11,
-            '4': 11,
-            '5': 11,
-            '6': 11,
+            '1': 10,
+            '2': 12,
+            '3': 12,
+            '4': 12,
+            '5': 12,
+            '6': 12,
             '7': 14,
-            '8': 11
+            '8': 12
           },
           'most-popular': {
             'name': 'Lemon',
@@ -201,14 +201,14 @@ var data = [
       'peanut': {
         'data': {
           'days': {
-            '1': 1,
-            '2': 21,
-            '3': 11,
-            '4': 11,
-            '5': 11,
-            '6': 11,
-            '7': 14,
-            '8': 11
+            '1': 13,
+            '2': 18,
+            '3': 14,
+            '4': 12,
+            '5': 13,
+            '6': 14,
+            '7': 15,
+            '8': 17
           },
           'most-popular': {
             'name': 'Bob',
@@ -225,26 +225,6 @@ var data = [
 
 ];
 
-var maxValue = 0,
-    numDays = 0;
-data.map(function(val, i) {
-  var subs = val.subsets;
-  for(sub in subs) {
-    console.log('days ',subs[sub].data['days']);
-    console.log('popular ',subs[sub].data['most-popular'].name);
-
-    var days = subs[sub].data['days'];
-    for (day in days) {
-      if(days[day] > maxValue) {
-        maxValue = days[day];
-      }
-      if(day > numDays) {
-        numDays = day;
-      }
-    }
-
-  }
-})
 
 var margin = { top: 20 , right: 30, bottom: 50, left: 60 },
     foo = {
@@ -260,13 +240,79 @@ var svg = d3.select("body")
     height: window.innerHeight
 });
 
+var line = d3.svg.line()
+    .x(function(d,i) { return x(d.x); }) // by this point, d is an Object like {x: 1960, y: "62.25436585"}
+    .y(function(d) { return y(d.y); });
+
+function getMaxVal() {
+  var maximum = 0;
+  data.forEach(function(val, i) {
+    var subs = val.subsets;
+    for(sub in subs) {
+      var days = subs[sub].data['days'];
+      for (day in days) {
+        if(days[day] > maximum) {
+          maximum = days[day];
+        }
+      }
+    } // sub in subs
+  })
+  return maximum;
+};
+
+function getNumDays() {
+  var numDays = 0;
+  data.forEach(function(val, i) {
+    var subs = val.subsets;
+    for(sub in subs) {
+      var days = subs[sub].data['days'];
+      for (day in days) {
+        if(day > numDays) {
+          numDays = day;
+        }
+      }
+    } // sub in subs
+  })
+  return numDays;
+};
+
+var maxValue = getMaxVal(),
+    numDays = getNumDays(),
+    y = d3.scale.linear().domain([maxValue, 0]).range([0, foo.height]),
+    x = d3.scale.linear().domain([0, numDays]).range([0, foo.width]);
+
+data.map(function(val, i) {
+  var subs = val.subsets;
+
+  for(sub in subs) {
+    var currData = [];
+
+    // console.log('days ',subs[sub].data['days']);
+    // console.log('popular ',subs[sub].data['most-popular'].name);
+
+    var days = subs[sub].data['days'];
+    for (day in days) {
+      currData.push({ x: day, y: days[day]});
+    }
+
+  d3.select('.names')
+    .append('svg:path')
+    .data([currData])
+    .attr('d', line)
+    .classed(sub, true)
+    .style({stroke: 'blue', fill: 'none'})
+
+  } // sub in subs
+
+
+})
+
+
+
 var heightScale = d3.scale.linear() // y = mx + b
   .domain([0, d3.max(maxValue)])
   .range([0, foo.height]);
 
-var line = d3.svg.line()
-    .x(function(d,i) { return x(d.x); }) // by this point, d is an Object like {x: 1960, y: "62.25436585"}
-    .y(function(d) { return y(d.y); });
 
 // Y axis
   var vGuideScale = d3.scale.linear()
@@ -286,8 +332,6 @@ var line = d3.svg.line()
   vGuide.attr('transform', 'translate(' + margin.left + ', ' + margin.top +')')
   vGuide.selectAll('path')
     .style({ fill: 'yellow', stroke: "blue", opacity: 0.3 });
-  vGuide.selectAll('line')
-    .style({ stroke: "red" })
 
 // X axis
   var hGuideScale = d3.scale.linear()
@@ -307,8 +351,6 @@ var line = d3.svg.line()
   hGuide.attr('transform', 'translate(' + margin.left + ', ' + (foo.height + margin.top) + ')')
   hGuide.selectAll('path')
     .style({ fill: 'green', stroke: "blue", opacity: 0.6 });
-  hGuide.selectAll('line')
-    .style({ stroke: "red" })
 
 
 }
